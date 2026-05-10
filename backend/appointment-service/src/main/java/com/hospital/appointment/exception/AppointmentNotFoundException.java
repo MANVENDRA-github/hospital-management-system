8078b8787e2de844bae3b736f0de3ff4bd8d1895
Manvendra <mksingh5904@gmail.com>
2026-05-10 21:57:59 +0530

    Add AppointmentNotFoundException in appointment-service

---
+++ backend/appointment-service/src/main/java/com/hospital/appointment/exception/AppointmentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hospital.appointment.exception;
+
+public class AppointmentNotFoundException extends RuntimeException {
+    public AppointmentNotFoundException(Long id) {
+        super("Appointment not found: " + id);
+    }
+}
