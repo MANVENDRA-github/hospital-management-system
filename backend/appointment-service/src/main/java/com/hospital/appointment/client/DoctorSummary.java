87b264f926e761331895cc9339b395ff1bbfe4eb
Manvendra <mksingh5904@gmail.com>
2026-05-10 21:35:57 +0530

    added ms 1

---
+++ backend/appointment-service/src/main/java/com/hospital/appointment/client/DoctorSummary.java
@@ -0,0 +1,18 @@
+package com.hospital.appointment.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DoctorSummary {
+    private Long id;
+    private String name;
+    private String specialization;
+}
